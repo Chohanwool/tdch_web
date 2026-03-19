a8284ed67a8ab2c662db10aab170591fbb4db9fc
Chohanwool <hanwoolc95@gmail.com>
2026-03-20 02:09:31 +0900

    design-담임목사 소개: 섹션1 죄측 위아래 패딩 추가

--- src/app/about/pastor/page.tsx
+++ src/app/about/pastor/page.tsx
@@ -59,7 +59,7 @@ function PastorHeroSection() {
 
       <div className="relative mx-auto w-full max-w-[1120px] px-4 py-10 md:px-8 md:py-14 lg:px-0 lg:py-0">
         <div className="relative grid min-h-[620px] items-end gap-8 px-2 pb-0 pt-10 md:min-h-[540px] md:px-2 md:pt-14 lg:h-[424px] lg:grid-cols-[minmax(0,0.95fr)_minmax(420px,0.98fr)] lg:items-start lg:gap-2 lg:px-0 lg:py-[36px]">
-          <div className="z-10 max-w-[460px] self-start pb-4 text-white md:pb-6 lg:flex lg:h-full lg:max-w-[420px] lg:self-stretch lg:flex-col lg:justify-between lg:pb-0 lg:pt-0">
+          <div className="z-10 max-w-[460px] self-start pb-4 pt-4 text-white md:pb-6 md:pt-4 lg:flex lg:h-full lg:max-w-[420px] lg:self-stretch lg:flex-col lg:justify-between lg:py-[15px]">
             <div>
               <div className="flex items-center gap-3 text-[#d5b25c]">
                 <span className="h-px w-10 bg-current md:w-12" />
